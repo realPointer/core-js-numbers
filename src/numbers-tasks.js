5f4967622012dbd85312ecd37d94d53d2ef58e38
Andrew <50529632+realPointer@users.noreply.github.com>
2024-12-14 23:29:08 +0300

    feat: complete task 16

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -289,8 +289,10 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return String(num)
+    .split('')
+    .reduce((sum, digit) => sum + Number(digit), 0);
 }
 
 /**
