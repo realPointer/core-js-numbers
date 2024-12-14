f42347addfdfca2d3ede1c55a8072e4c8ed5c8c7
Andrew <50529632+realPointer@users.noreply.github.com>
2024-12-14 23:26:07 +0300

    feat: complete task 14

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -257,8 +257,10 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  return index <= 1
+    ? index
+    : getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
 }
 
 /**
